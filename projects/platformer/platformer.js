c8ef5220dc9d558571b457ceaf1e425987d10b4d
Brenda Herrera <brendaherrera421@gmail.com>
2023-07-10 19:19:53 +0000

    added last platforms right

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,17 +47,39 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    //A step
+    createPlatform(200, 630, 80, 10)
+    //B step
+    createPlatform(300, 530, 80, 10)
+    //C step
+    createPlatform(400, 430, 80, 10)
+    //D platform with walls
+    createPlatform(500, 330, 210, 10)
+    createPlatform(560, 230, 10, 100)
+    createPlatform(640, 230, 10, 100)
+    //E left long platform
+    createPlatform(0, 330, 300, 10)
+    //F step 2 side platform
+    createPlatform(30, 530, 100, 10)
+    //G platform right of database collectable
+    createPlatform(900, 330, 100, 10)
+    //H platform top to bottom 1
+    createPlatform(1000, 430, 100, 10)
+    //I platform top to bottom 2
+    createPlatform(1100, 530, 100, 10)
+    //J platform bottom right last coll
+    createPlatform(1200, 630, 200, 10)
+    //K platform wall bottom right
+    createPlatform(1320, 530, 10, 100)
 
 
-
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("database", 585, 290);
+    createCollectable("steve", 60, 450, 10, 0.5);
 
 
     // TODO 3
@@ -65,7 +87,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("top", 580, 2000)
+    createCannon("top", 750, 2000)
+    createCannon("bottom",100,1000)
 
 
 
