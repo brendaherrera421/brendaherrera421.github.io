bdda34b62853d7c54109f46cfbbd8a28c087f5c1
brendaherrera421 <138803354+brendaherrera421@users.noreply.github.com>
2024-07-17 15:19:51 +0000

    completed walker but up down does not work

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,26 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var BOARD_WIDTH = $("#board").width();
+  var BOARD_HEIGHT = $("#board").height();
+  var KEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40,
+  }
   // Game Item Objects
-
+  var walker = {
+    "coordinateX": 0,
+    "coordinateY": 0,
+    "speedX": 0,
+    "speedY": 0,
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +40,69 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    redrawGameItem();
+    repositionGameItem();
+    wallCollision();
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
 
+  function handleKeyDown(event) {
+    changeSpeedX(-5, event.which, KEY.LEFT);
+    changeSpeedX(5, event.which, KEY.RIGHT);
+    changeSpeedY(-5, event.which, KEY.UP);
+    changeSpeedY(5, event.which, KEY.DOWN);
+  }
+
+  function handleKeyUp(event) {
+    changeSpeedX(0, event.which, KEY.LEFT);
+    changeSpeedX(0, event.which, KEY.RIGHT);
+    changeSpeedY(0, event.which, KEY.UP);
+    changeSpeedY(0, event.which, KEY.DOWN);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function changeSpeedX(newSpeed, keycode, arrowKey){
+    if (keycode === arrowKey) {
+      walker.speedX = newSpeed;
+    }
+  }
 
+  function changeSpeedY(newSpeed, keycode, arrowKey){
+    if (keycode === arrowKey) {
+      walker.speedY = newSpeed;
+    }
+  }
   
+  function repositionGameItem(){
+    walker.coordinateX += walker.speedX;
+    walker.coordinateY += walker.speedY;
+  }
+
+  function redrawGameItem(){
+    $("#walker").css("left", walker.coordinateX);
+    $("#walker").css("right", walker.coordinateY);
+  }
+  
+  function wallCollision(){
+    if(walker.coordinateX > BOARD_WIDTH - $("#walker").width()){
+      walker.coordinateX = BOARD_WIDTH - $("#walker").width();
+    }
+    else if(walker.coordinateX < 0){
+      walker.coordinateX = 0;
+    }
+    if(walker.coordinateY > BOARD_HEIGHT - $("#walker").height()){
+      walker.coordinateY = BOARD_HEIGHT - $("#walker").height();
+    }
+    else if(walker.coordinateY < 0){
+      walker.coordinateY = 0;
+    }
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
